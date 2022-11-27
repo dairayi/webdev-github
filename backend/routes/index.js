e06cbbd98f9d6359789c8ba9abb11863af955b83
dairayi <72094446+dairayi@users.noreply.github.com>
2022-11-27 13:56:10 +0000

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,13 +4,73 @@ let Driver = require('../models/driver');
 let Circuit = require('../models/circuit');
 let Constructor = require('../models/constructor')
 let mongoose = require('mongoose');
+let User = require("../models/user");
 
 var router = express.Router();
+
+// router.get('/user', (req, res, next) => {
+//     let searchQuery = {};
+//
+//     searchQuery = { "email": { $regex: req.query.email, $options: 'i' } };
+//
+//     User.find(searchQuery, (err, users) => {
+//         if (err) {
+//             res.status(400);
+//             res.send();
+//         }
+//
+//         // Could do a 204 or 404 for no results
+//         // but the reponse is a success
+//         // and the filter results are accurate
+//
+//         res.status(200);
+//         res.send(users);
+//     })
+// });
+
+router.get('/user', (req, res, next) => {
+    let searchQuery = {};
+
+    searchQuery = { "username": { $regex: req.query.username, $options: 'i' } };
+
+    User.findOne(searchQuery, (err, users) => {
+        if (err) {
+            res.status(400);
+            res.send();
+        }
+
+        // Could do a 204 or 404 for no results
+        // but the reponse is a success
+        // and the filter results are accurate
+
+        res.status(200);
+        res.send(users);
+    })
+
+});
+
+
+router.post('/user', (req, res, next) => {
+    const newUser = new User(req.body);
+
+    newUser._id = mongoose.Types.ObjectId();
+
+    newUser.save((err) => {
+        if (err) {
+            res.status(400);
+            res.send();
+        } else {
+            res.status(201);
+            res.send({ id : newUser._id });
+        }
+
+    });
+});
 
 router.get('/constructor', (req, res, next) => {
     let searchQuery = {};
 
-    searchQuery = { "constructorRef": { $regex: req.query.constructorRef, $options: 'i' } };
+    searchQuery = { "name": { $regex: req.query.name, $options: 'i' } };
 
     Constructor.find(searchQuery, (err, constructors) => {
         if (err) {
@@ -30,7 +90,7 @@ router.get('/constructor', (req, res, next) => {
 router.get('/driver', (req, res, next) => {
     let searchQuery = {};
 
-    searchQuery = { "driverRef": { $regex: req.query.driverRef, $options: 'i' } };
+    searchQuery = { "surname": { $regex: req.query.surname, $options: 'i' } };
 
     Driver.find(searchQuery, (err, drivers) => {
         if (err) {
@@ -50,7 +110,7 @@ router.get('/driver', (req, res, next) => {
 router.get('/circuit', (req, res, next) => {
     let searchQuery = {};
 
-    searchQuery = { "circuitRef": { $regex: req.query.circuitRef, $options: 'i' } };
+    searchQuery = { "name": { $regex: req.query.name, $options: 'i' } };
 
     Circuit.find(searchQuery, (err, circuits) => {
         if (err) {

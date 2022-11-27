6f407d57a9f88ef547ba259b60ff8f69ae3bed6c
dairayi <72094446+dairayi@users.noreply.github.com>
2022-11-27 13:55:11 +0000

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -10,6 +10,9 @@ let Schema = mongoose.Schema;
 let userSchema = new Schema({
     username: String,
     password: String
+    favdriver: String,
+    favconstructor: String,
+    favcircuit: String
 });
 let User = mongoose.model('User', userSchema);
 module.exports = User;
